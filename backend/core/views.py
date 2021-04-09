108926d6a8baa08e1dd2f62dbfc81de71ac40d57
handreygalon <handrey.eg@gmail.com>
2021-04-09 01:37:06 -0300

    Validate event when a communication is added and CPF and email validation

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 
 from rest_framework.response import Response
 from rest_framework import viewsets
@@ -19,27 +20,51 @@ class CommViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)'''
 
     def create(self, request, *args, **kwargs):
+        print("POST")
         comm_data = request.data
         requestLat = comm_data["latitude"]
         requestLon = comm_data["longitude"]
         requestHarvestDate = comm_data["harvestDate"]
         latitudeList, longitudeList, validPositionList = [], [], []
 
-        if CropLossComm.objects.filter(harvestDate=requestHarvestDate).exists():
+        event = Event.objects.get(id=comm_data["event"])
+
+        # print(event)
+
+        mesmaData = CropLossComm.objects.filter(harvestDate=requestHarvestDate)
+        mesmaData = mesmaData.filter(~Q(event=event))
+
+        print(mesmaData)
+
+        if mesmaData.exists():
             validDate = True
         else:
             validDate = False
+        # print(validDate)
 
-        for i in range(len(self.queryset)):
-            latitudeList.append(self.queryset[i].latitude)            
-            longitudeList.append(self.queryset[i].longitude)
+        '''for item in self.queryset:
+            print(item.event)
+            validPositionList.append(utils.haversine(item.latitude, item.longitude, requestLon, requestLat))'''
+        '''if item.event.id == comm_data["event"]:
+            validEvent = True
+        else:
+            validEvent = False'''
+
+        for i in range(len(mesmaData)):
+
+            # print(mesmaData[i].event)
+
+            latitudeList.append(mesmaData[i].latitude)            
+            longitudeList.append(mesmaData[i].longitude)
             validPositionList.append(utils.haversine(longitudeList[i], latitudeList[i], requestLon, requestLat))
+            
+        # print(validPositionList)
 
         if any(x == True for x in validPositionList) and validDate:
             return Response(status=status.HTTP_417_EXPECTATION_FAILED)
 
         cultivation = Cultivation.objects.get(id=comm_data["cultivation"])
-        event = Event.objects.get(id=comm_data["event"])
+        
         new_comm = CropLossComm.objects.create(name=comm_data["name"],
                                                email=comm_data["email"],
                                                cpf=comm_data["cpf"],
@@ -50,6 +75,28 @@ class CommViewSet(viewsets.ModelViewSet):
                                                harvestDate=comm_data["harvestDate"])
         new_comm.save()
         serializer = CropLossCommSerializer(new_comm)
+        return Response(serializer.data)
+    
+    def update(self, request, *args, **kwargs):
+        print("PUT")
+        comm_data = request.data
+        print(kwargs['pk'])
+        comm = CropLossComm.objects.get(id=kwargs['pk'])
+
+        cultivation = Cultivation.objects.get(id=comm_data["cultivation"])
+        event = Event.objects.get(id=comm_data["event"])
+
+        comm.name = comm_data["name"]
+        comm.email = comm_data["email"]
+        comm.cpf = comm_data["cpf"]
+        comm.latitude = comm_data["latitude"]
+        comm.longitude = comm_data["longitude"]
+        comm.cultivation = cultivation
+        comm.event = event
+        comm.harvestDate = comm_data["harvestDate"]
+
+        comm.save()
+        serializer = CropLossCommSerializer(comm)
         return Response(serializer.data)
 
 
